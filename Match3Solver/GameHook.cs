7d145ec4a48659a1e0d714128dcf9e1a50ae2bf1
mdnpascual <mdnpascual@yahoo.com.ph>
2021-02-24 20:09:49 -0700

    Commit before testing out dx11

--- Match3Solver/GameHook.cs
+++ Match3Solver/GameHook.cs
@@ -12,6 +12,21 @@ using System.Threading;
 using System.Windows.Controls;
 using System.Drawing;
 
+using SharpDX.Direct2D1;
+using Factory = SharpDX.Direct2D1.Factory;
+using FontFactory = SharpDX.DirectWrite.Factory;
+using Format = SharpDX.DXGI.Format;
+using SharpDX;
+using SharpDX.DirectWrite;
+using Bitmap = System.Drawing.Bitmap;
+using Rectangle = System.Drawing.Rectangle;
+using Color = SharpDX.Color;
+using TextAntialiasMode = SharpDX.Direct2D1.TextAntialiasMode;
+using FontStyle = SharpDX.DirectWrite.FontStyle;
+using RectangleF = SharpDX.RectangleF;
+
+using SharpDX.Direct3D11;
+
 namespace Match3Solver
 {
     public class GameHook
@@ -23,6 +38,14 @@ namespace Match3Solver
 
         TextBlock message;
 
+        private Thread sDX = null;
+        private WindowRenderTarget device;
+        private HwndRenderTargetProperties renderProperties;
+        private Factory factory;
+        private FontFactory fontFactory;
+        private IntPtr targetWindow;
+        private TextFormat textFormat;
+
         public GameHook(TextBlock statusMessage)
         {
             this.message = statusMessage;
@@ -31,7 +54,8 @@ namespace Match3Solver
         public void AttachProcess()
         {
 
-            Process[] processes = Process.GetProcessesByName("HuniePop 2 - Double Date");
+            //Process[] processes = Process.GetProcessesByName("HuniePop 2 - Double Date");
+            Process[] processes = Process.GetProcessesByName("vlc");
             foreach (Process process in processes)
             {
                 // Simply attach to the first one found.
@@ -58,6 +82,7 @@ namespace Match3Solver
 
                 processId = process.Id;
                 _process = process;
+                targetWindow = process.MainWindowHandle;
 
                 var captureInterface = new CaptureInterface();
                 captureInterface.RemoteMessage += new MessageReceivedEvent(CaptureInterface_RemoteMessage);
@@ -77,6 +102,32 @@ namespace Match3Solver
                 this.message.Text = "Attached to game";
                 hooked = true;
             }
+
+            //////////////
+            factory = new Factory();
+            fontFactory = new FontFactory();
+            textFormat = new TextFormat(fontFactory, "Arial",
+                FontWeight.Normal, FontStyle.Normal, 32.0f)
+            {
+                TextAlignment = TextAlignment.Center,
+                ParagraphAlignment = ParagraphAlignment.Center
+            };
+            renderProperties = new HwndRenderTargetProperties()
+            {
+                Hwnd = this.targetWindow,
+                PixelSize = new Size2(800, 600),
+                PresentOptions = PresentOptions.None,
+            };
+
+            //Init DirectX
+            device = new WindowRenderTarget(factory, new RenderTargetProperties(new PixelFormat(Format.B8G8R8A8_UNorm, AlphaMode.Premultiplied)), renderProperties);
+            sDX = new Thread(new ParameterizedThreadStart(sDXThread));
+            sDX.Priority = ThreadPriority.Highest;
+            sDX.IsBackground = true;
+            sDX.Start();
+            //////////////
+
+            //SharpDX.Direct3D11.Device device11 = new SharpDX.Direct3D11.Device(this.targetWindow);
         }
 
         public Bitmap getScreenshot()
@@ -92,6 +143,25 @@ namespace Match3Solver
         void CaptureInterface_RemoteMessage(MessageReceivedEventArgs message)
         {
             Console.WriteLine(message.Message);
+        }
+
+        private void sDXThread(object sender)
+        {
+            while (true)
+            {
+                device.BeginDraw();
+                device.Clear(Color.Transparent);
+                device.TextAntialiasMode = TextAntialiasMode.Aliased;// you can set another text mode
+
+                //device.DrawRectangle(new RectangleF(0, 0, 400, 400), new SolidColorBrush(device, Color.Pink));
+                device.DrawText("LKJDSAGHFKLJASGDFKAGSDKFASDF", textFormat, new RectangleF(0, 0, 400, 400), new SolidColorBrush(device, Color.Pink));
+
+                //place your rendering things here
+
+                device.EndDraw();
+            }
+
+            //whatever you want
         }
     }
 }
